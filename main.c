871f8a065e491083d35603ac01290bc178855112
Paul BEZIAU <paul.beziau@laposte.net>
2016-11-23 20:03:14 +0100

    correction du nombre d'assertion

--- main.c
+++ main.c
@@ -19,37 +19,38 @@ int getVar(int nbColor,int numVertice,int numColor)
 int main(int argc, char** argv)
 {
 #ifdef NDEBUG
-  int nbVertice;
+    int nbVertice;
 #endif
-  int nbColor=atoi(argv[1]);
-  nbVertice=orderG();
-  int nbClose=orderG()+orderG()*nbColor*nbColor+sizeG()*nbColor;
-  printf("p cnf %d %d\n",nbVertice*nbColor,nbClose);
-  #if 1
-  for(int i=0;i<nbVertice;i++)
+    int nbColor=atoi(argv[1]);
+    int n=nbColor-1;
+    nbVertice=orderG();
+    int nbClose=orderG()+nbVertice*(n*n+n)/2+sizeG()*nbColor;
+    printf("p cnf %d %d\n",nbVertice*nbColor,nbClose);
+#if 1
+    for(int i=0;i<nbVertice;i++)
     {
-      for(int j=0;j<nbColor;j++)
-	printf("%d ",getVar(nbColor,i,j));
-      printf("0\n");
-      for(int j=0;j<nbColor;j++)
-	for(int k=j+1;k<nbColor;k++)
-	  printf("%d %d 0\n",-getVar(nbColor,i,j),-getVar(nbColor,i,k));
+	for(int j=0;j<nbColor;j++)
+	    printf("%d ",getVar(nbColor,i,j));
+	printf("0\n");
+	for(int j=0;j<nbColor;j++)
+	    for(int k=j+1;k<nbColor;k++)
+		printf("%d %d 0\n",-getVar(nbColor,i,j),-getVar(nbColor,i,k));
     }
-  #endif
-  #if 1
-  for(int i=0;i<nbVertice;i++)
+#endif
+#if 1
+    for(int i=0;i<nbVertice;i++)
     {
-      for(int j=i;j<nbVertice;j++)
+	for(int j=i;j<nbVertice;j++)
 	{
-	  if(are_adjacent(i,j))
-	     for(int k=0;k<nbColor;k++)
-	       {
-		 int var1=-getVar(nbColor,i,k);
-		 int var2=-getVar(nbColor,j,k);
-		 printf("%d %d 0\n",var1,var2);
-	       }
+	    if(are_adjacent(i,j))
+		for(int k=0;k<nbColor;k++)
+		{
+		    int var1=-getVar(nbColor,i,k);
+		    int var2=-getVar(nbColor,j,k);
+		    printf("%d %d 0\n",var1,var2);
+		}
 	}
     }
 #endif			
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
